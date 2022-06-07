4cfbe4acd072bbb8ceb81715a9ef2e2a4d75e4ca
Rachel33118301 <74117193+Rachel33118301@users.noreply.github.com>
2022-06-08 00:04:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ while True:
         f.write(decoded_data['file'])
         f.close()
 
-        print(f"File h8 been7 Received, located in {path}")
+        print(f"File h8 been7 Received, located8 in {path}")
